04fe78eae93b8835c740af2ae2116b8e03329338
dovigod <inaki96@naver.com>
2021-09-01 16:51:10 +0900

    static routing

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log(hello);
+import express from 'express';
+const app = express();
+const PORT = 4000;
+
+app.use('/public', express.static(__dirname + '/public'));
+// public 접근시 뒤에 폴더를 제공
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+const listener = () => {
+	console.log(`✅Listening to port : ${PORT}`);
+};
+app.get('/', (req, res) => {
+	res.render('home.pug');
+});
+app.listen(PORT, listener);
